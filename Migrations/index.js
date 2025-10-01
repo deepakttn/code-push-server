353a5ce3e5c5483fadd2d542b0f6fe35639f1263
Deepak <deepak@tothenew.com>
2025-10-01 22:13:46 +0530

    contentful code updated

--- Migrations/index.js
+++ Migrations/index.js
@@ -1,4 +1,25 @@
 module.exports = function (migration) {
   const blogPost = migration.editContentType('blogPost');
-  blogPost.createField('FieldKeyName').name('SummaryKeyName').type('Text');
+
+  blogPost.createField('summaryKeyName')
+    .name('Summary')
+    .type('Text');
+
+  blogPost.createField('author')
+    .name('Author')
+    .type('Symbol');
+
+  blogPost.createField('publishDate')
+    .name('Publish Date')
+    .type('Date');
+
+  blogPost.createField('tags')
+    .name('Tags')
+    .type('Array')
+    .items({ type: 'Symbol' });
+
+  blogPost.createField('featuredImage')
+    .name('Featured Image')
+    .type('Link')
+    .linkType('Asset');
 };
